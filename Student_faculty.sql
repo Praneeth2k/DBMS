67d75d0f9777335aee734ddfea736e972389c1bb
Praneeth2k <47350983+Praneeth2k@users.noreply.github.com>
2020-03-06 07:15:32 +0530

    Create Student_faculty.sql

---
+++ Student_faculty.sql
@@ -0,0 +1,23 @@
+create table STUDENT(snum int, sname varchar(40), major varchar(40), lvl varchar(4),age int, PRIMARY KEY(snum));
+
+create table FACULTY(fid int,fname varchar(40), deptid int,PRIMARY KEY(fid));
+
+create table CLASS(cname varchar(40),meets_at time,room varchar(40),fid int,Primary key(cname), FOREIGN KEY(fid) references FACTULTY(fid) on delete cascade);
+
+create table ENROLLED(snum int,cname varchar(40),PRIMARY KEY(snum,cname),FOREIGN KEY(snum) REFERENCES STUDENT(snum),FOREIGN KEY(cname) REFERENCES CLASS(cname) on delete cascade);
+
+insert into STUDENT values(019,'Arjun','CSE','MD',19);
+insert into STUDENT values(013,'Amber','CSE','MD',11);
+insert into STUDENT values(023,'Aditya','ECE','SR',20);
+insert into STUDENT values(041,'Shashank','ME','SS',21);
+insert into STUDENT values(051,'Navaneet','ISE','JR',18);
+select * from STUDENT;
+
+insert into FACULTY values(020,'NV',001),(030,'GS',002),(026,'HF',002),(044,'VJ',003),(041,'BJ',001),(099,'VJ',003),(011,'KVN',001);
+select * from FACULTY;
+
+insert into CLASS values('DBMS','11:15:00','203',011),('ED','10;00;00','211',044),('AI','01:30;00','022',041),('TFC','02:20:10','100',002);
+select * from CLASS;
+
+insert into enrolled values(19,'DBMS'),(13,'DBMS'),(023,'TFC'),(41,'AI'),(51,'ED');
+select * from enrolled;
